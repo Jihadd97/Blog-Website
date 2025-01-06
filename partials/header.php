dccfce63bb73c162d7766f238bafb7b0295794b3
Jihad Ahmed <128065661+Jihadd97@users.noreply.github.com>
2025-01-06 02:20:08 +0200

    Add files via upload

---
+++ partials/header.php
@@ -0,0 +1,54 @@
+<?php
+require 'config/database.php';
+
+//fetch current user from database
+if(isset($_SESSION['userId'])){
+    $id = $_SESSION['userId'];
+    $query = "SELECT avatar FROM users WHERE id=$id";
+    $result = mysqli_query($connection, $query);
+    $avatar = mysqli_fetch_assoc( $result );
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP & MySQL Blog</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="container nav_container">
+            <a href="<?= ROOT_URL ?>index.php" class="nav_logo">Traveler</a>
+            <ul class="nav_items show">
+                <li><a href="<?= ROOT_URL ?>blog.php">Blog</a></li>
+                <li><a href="<?= ROOT_URL ?>about.php">About</a></li>
+                <li><a href="<?= ROOT_URL ?>services.php">Services</a></li>
+                <li><a href="<?= ROOT_URL ?>contact.php">Contact</a></li>
+                <?php if (isset($_SESSION['userId'])): ?>
+                    <li class="nav_profile">
+                        <div class="avatar">
+                            <img src="<?= ROOT_URL . 'images/'. $avatar['avatar']?>" alt="profile1">
+                        </div>
+                        <ul>
+                            <li><a href="<?= ROOT_URL ?>admin/index.php">Dashboard</a></li>
+                            <li><a href="<?= ROOT_URL ?>logout.php">Logout</a></li>
+                        </ul>
+                    </li>
+                <?php else : ?>
+                    <li><a href="<?= ROOT_URL ?>login.php">Login</a></li>
+                <?php endif ?>
+
+            </ul>
+            <button id="open_nav-btn"><i class="fa-solid fa-bars"></i></button>
+            <button id="close_nav-btn"><i class="fa-solid fa-xmark"></i></button>
+        </div>
+    </nav>
+
+    <!------------------------------- END OF NAV ------------------------------------>
